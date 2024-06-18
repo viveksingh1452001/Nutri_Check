cb022321a7a08b305638ce9d13a02e7fde156b30
Vivek <vivekkushvaha125@gmail.com>
2024-06-18 16:30:47 +0530

    add cors package in server

--- server/index.js
+++ server/index.js
@@ -1,17 +1,14 @@
 const express = require('express');
-
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 require('dotenv').config();
 
 
 const port= process.env.PORT || 3000;
 
 app.listen(port,()=>{
-    try {
         console.log(`server running on port:${port}`);
-        
-    } catch (error) {
-        console.log(error);
-    }
-    
 })
